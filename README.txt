03d36a43f57b35212bfcc56785adf795160db68f
m16a <mshvorak@gmail.com>
2022-02-20 19:40:40 +0200

    physical device was picked

---
+++ README.txt
@@ -0,0 +1,4 @@
+Links:
+https://kylemayes.github.io/vulkanalia
+https://github.com/unknownue/vulkan-tutorial-rust using ash
+https://github.com/bwasty/vulkan-tutorial-rs
--- src/render/vulkan.rs
+++ src/render/vulkan.rs
@@ -4,6 +4,9 @@ use crate::utils::debug::ValidationInfo;
 
 use ash::vk::{self, API_VERSION_1_2};
 
+#[allow(deprecated)]
+use ash::vk::{version_major, version_minor, version_patch};
+
 use std::ffi::{CStr, CString};
 use std::os::raw::c_void;
 use std::ptr;
@@ -44,11 +47,22 @@ unsafe extern "system" fn vulkan_debug_utils_callback(
 	vk::FALSE
     }
 
+struct QueueFamilyIndices {
+graphics_family: Option<u32>,
+}
+
+impl QueueFamilyIndices {
+pub fn is_complete(&self) -> bool {
+	self.graphics_family.is_some()
+}
+}
+
 pub struct Vulkan {
 	_entry: ash::Entry,
 	instance: ash::Instance,
 	debug_utils_loader: ash::extensions::ext::DebugUtils,
 	debug_messanger: vk::DebugUtilsMessengerEXT,
+	_physical_device: vk::PhysicalDevice,
 }
 
 impl Vulkan {
@@ -58,7 +72,8 @@ impl Vulkan {
 		let entry = ash::Entry::new().unwrap();
 		let instance = Vulkan::create_instance(&entry);
 		let (debug_utils_loader, debug_messanger) = Vulkan::setup_debug_utils(&entry, &instance);
-		Vulkan{_entry : entry, instance, debug_utils_loader, debug_messanger}
+		let physcial_device = Vulkan::pick_physical_device(&instance);
+		Vulkan{_entry : entry, instance, debug_utils_loader, debug_messanger, _physical_device : physcial_device}
 		}
 	}
 
@@ -124,7 +139,150 @@ impl Vulkan {
 		
 			instance
 	    }
-	    
+	    fn pick_physical_device(instance: &ash::Instance) -> vk::PhysicalDevice {
+		let physical_devices = unsafe {
+		    instance
+			.enumerate_physical_devices()
+			.expect("Failed to enumerate Physical Devices!")
+		};
+	
+		println!(
+		    "{} devices (GPU) found with vulkan support.",
+		    physical_devices.len()
+		);
+	
+		let mut result = None;
+		for &physical_device in physical_devices.iter() {
+		    if Vulkan::is_physical_device_suitable(instance, physical_device) {
+			if result.is_none() {
+			    result = Some(physical_device)
+			}
+		    }
+		}
+	
+		match result {
+		    None => panic!("Failed to find a suitable GPU!"),
+		    Some(physical_device) => physical_device,
+		}
+	    }
+	
+	    fn is_physical_device_suitable(
+		instance: &ash::Instance,
+		physical_device: vk::PhysicalDevice,
+	    ) -> bool {
+		let device_properties = unsafe { instance.get_physical_device_properties(physical_device) };
+		let device_features = unsafe { instance.get_physical_device_features(physical_device) };
+		let device_queue_families =
+		    unsafe { instance.get_physical_device_queue_family_properties(physical_device) };
+	
+		let device_type = match device_properties.device_type {
+		    vk::PhysicalDeviceType::CPU => "Cpu",
+		    vk::PhysicalDeviceType::INTEGRATED_GPU => "Integrated GPU",
+		    vk::PhysicalDeviceType::DISCRETE_GPU => "Discrete GPU",
+		    vk::PhysicalDeviceType::VIRTUAL_GPU => "Virtual GPU",
+		    vk::PhysicalDeviceType::OTHER => "Unknown",
+		    _ => panic!(),
+		};
+	
+		let device_name = utils::tools::vk_to_string(&device_properties.device_name);
+		println!(
+		    "\tDevice Name: {}, id: {}, type: {}",
+		    device_name, device_properties.device_id, device_type
+		);
+		#[allow(deprecated)]
+		let major_version = version_major(device_properties.api_version);
+		#[allow(deprecated)]
+		let minor_version = version_minor(device_properties.api_version);
+		#[allow(deprecated)]
+		let patch_version = version_patch(device_properties.api_version);
+	
+		println!(
+		    "\tAPI Version: {}.{}.{}",
+		    major_version, minor_version, patch_version
+		);
+	
+		println!("\tSupport Queue Family: {}", device_queue_families.len());
+		println!("\t\tQueue Count | Graphics, Compute, Transfer, Sparse Binding");
+		for queue_family in device_queue_families.iter() {
+		    let is_graphics_support = if queue_family.queue_flags.contains(vk::QueueFlags::GRAPHICS)
+		    {
+			"support"
+		    } else {
+			"unsupport"
+		    };
+		    let is_compute_support = if queue_family.queue_flags.contains(vk::QueueFlags::COMPUTE) {
+			"support"
+		    } else {
+			"unsupport"
+		    };
+		    let is_transfer_support = if queue_family.queue_flags.contains(vk::QueueFlags::TRANSFER)
+		    {
+			"support"
+		    } else {
+			"unsupport"
+		    };
+		    let is_sparse_support = if queue_family
+			.queue_flags
+			.contains(vk::QueueFlags::SPARSE_BINDING)
+		    {
+			"support"
+		    } else {
+			"unsupport"
+		    };
+	
+		    println!(
+			"\t\t{}\t    | {},  {},  {},  {}",
+			queue_family.queue_count,
+			is_graphics_support,
+			is_compute_support,
+			is_transfer_support,
+			is_sparse_support
+		    );
+		}
+	
+		// there are plenty of features
+		println!(
+		    "\tGeometry Shader support: {}",
+		    if device_features.geometry_shader == 1 {
+			"Support"
+		    } else {
+			"Unsupport"
+		    }
+		);
+	
+		let indices = Vulkan::find_queue_family(instance, physical_device);
+	
+		return indices.is_complete();
+	    }
+	
+	    fn find_queue_family(
+		instance: &ash::Instance,
+		physical_device: vk::PhysicalDevice,
+	    ) -> QueueFamilyIndices {
+		let queue_families =
+		    unsafe { instance.get_physical_device_queue_family_properties(physical_device) };
+	
+		let mut queue_family_indices = QueueFamilyIndices {
+		    graphics_family: None,
+		};
+	
+		let mut index = 0;
+		for queue_family in queue_families.iter() {
+		    if queue_family.queue_count > 0
+			&& queue_family.queue_flags.contains(vk::QueueFlags::GRAPHICS)
+		    {
+			queue_family_indices.graphics_family = Some(index);
+		    }
+	
+		    if queue_family_indices.is_complete() {
+			break;
+		    }
+	
+		    index += 1;
+		}
+	
+		queue_family_indices
+	    }
 	    #[allow(dead_code)]
 	    fn check_validation_layer_support(entry: &ash::Entry) -> bool {
 		// if support validation layer, then return true
